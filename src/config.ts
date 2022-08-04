e0a32c029cd2a5d612c75eda1abe4c28ce266e17
malikfaizan111 <36520789+malikfaizan111@users.noreply.github.com>
2022-08-04 11:51:14 +0500

    add config file

---
+++ src/config.ts
@@ -0,0 +1,78 @@
+export const appConfig =
+{
+   //-----------------------------INTERNAL URLs---------------------------//
+
+  //  base_url: 'http://internal.adminurban.com/UrbanPoint-backend/api/v1/',
+  //  file_url: 'http://internal.adminurban.com/uploads/',
+
+  //  base_urlV2: 'http://internal.adminurban.com/up_qatar_v2/api/v2/',
+  //  file_urlV2: 'https://d35lr39dcxnup6.cloudfront.net/uploads_staging/uploads/',
+
+  //-----------------------------STAGING URLs---------------------------//
+
+
+
+    // base_url: 'http://staging.adminurban.com/up_qatar/api/v1/',
+    // base_urlV2: 'http://staging.adminurban.com/up_qatar_v2/api/v2/',
+    
+    // file_url: 'http://staging.adminurban.com/uploads/',
+    // file_urlV2: 'https://d35lr39dcxnup6.cloudfront.net/uploads_staging/uploads/',
+
+
+      base_url : 'https://staging-v1.adminurban.com/api/v1/',
+      base_urlV2: 'https://staging-v2.adminurban.com/api/v2/',
+
+       file_url: 'https://urbanpoint-storage.azureedge.net/test/uploads/',
+       file_urlV2: 'https://urbanpoint-storage.azureedge.net/test/uploads_staging/uploads/',
+
+
+    //-----------------------------PRE-PRODUCTION URLs---------------------------//
+
+    // base_url: 'http://testing.adminurban.com/UrbanPoint-backend/api/v1/',
+    // file_url: 'https://adminurban.com/up_qatar/uploads/',
+
+    // base_urlV2: 'http://testing.adminurban.com/up/api/v2/',
+    // file_urlV2: 'https://d35lr39dcxnup6.cloudfront.net/uploads/',
+
+  //-----------------------------LIVE URLs---------------------------//
+
+  // base_url: 'https://adminurban.com/up_backend/api/v1/',
+  // file_url: 'https://adminurban.com/up_qatar/uploads/',
+
+  // // base_urlV2: 'https://adminurban.com/up_qatar_v2/api/v2/',
+  // base_urlV2: 'https://cms-api.adminurban.com/api/v2/',
+  // file_urlV2: 'https://d35lr39dcxnup6.cloudfront.net/uploads/',
+
+  //------------------------------END--------------------------------//
+
+
+    // // Staging
+
+    // (hanif change in to s3 bucket)
+    // file_url: 'https://d35lr39dcxnup6.cloudfront.net/uploads_staging/uploads/',
+
+    // base_url: 'https://orders.adminurban.com/up_qatar/api/v1/',
+    // file_url: 'https://d35lr39dcxnup6.cloudfront.net/uploads/',
+
+    // // Staging
+    // file_urlV2: 'http://staging.adminurban.com/up_qatar_v2/uploads/',
+    // file_urlV2: 'https://d35lr39dcxnup6.cloudfront.net/uploads_staging/uploads/',(again change according to ali pasha)
+
+
+
+    //Live
+    // base_url: 'http://18.185.217.28/up_qatar/api/v1/',
+    // file_url: 'http://18.185.217.28/up_qatar/uploads/',
+    // file_url: 'https://d35lr39dcxnup6.cloudfront.net/uploads/',(ali gave this file url for v2 api)
+
+
+    // // ADminUrban
+
+    base_url_slug: 'cms/',
+    currency: 'QAR',
+
+    default_auth_key: 'UP!and$',
+    // google_api_key: 'AIzaSyBWA0K0LkWH5Q2owT6Kh7uHBO-pJl7o7nk',
+    google_api_key: 'AIzaSyAn5pLKFcSFwQWkMLNcDZEReqxf8hp-pxY'
+    // google_api_key: 'AIzaSyBWA0K0LkWH5Q2owT6Kh7uHBO-pJl7o7nk'
+}
