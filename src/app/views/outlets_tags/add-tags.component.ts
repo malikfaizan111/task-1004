54c0332106a4afe9c1ff1bb3453671b32f3aa0c7
maz1993 <104066578+maz1993@users.noreply.github.com>
2022-10-22 14:29:07 -0700

    add tags layout done

--- src/app/views/outlets_tags/add-tags.component.ts
+++ src/app/views/outlets_tags/add-tags.component.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-tags',
   templateUrl: './add-tags.component.html',
   styles: [
+    '.tag_btn[disabled] {color: #1F1F1F}'
   ]
 })
 export class AddTagsComponent implements OnInit {
 
+  form: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.addNewTags()
   }
+
+
+  addNewTags()
+  {
+    this.form = new FormGroup({
+      'tag_category': new FormControl('', Validators.required),
+      'tag_name': new FormControl('', Validators.required)
+    })
+  }
+
+  getValue(name: any) {
+    return this.form.get(name);
+}
+
+
+  onLocationBack(): void {
+    window.history.back();
+}
+
+doSubmit()
+{
+  console.log('form values...', this.form.value)
+}
 
 }
