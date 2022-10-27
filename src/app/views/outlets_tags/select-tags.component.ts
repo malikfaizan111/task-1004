a325082dbc53daae38c8271a6a4afd7b622c755d
maz1993 <104066578+maz1993@users.noreply.github.com>
2022-10-27 12:32:21 -0700

    all done

--- src/app/views/outlets_tags/select-tags.component.ts
+++ src/app/views/outlets_tags/select-tags.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-select-tags',
@@ -10,73 +11,181 @@ import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 })
 export class SelectTagsComponent implements OnInit {
 
-  form: FormGroup = new FormGroup({})
-  arr: Array<any> = []
-  constructor(private formBuilder : FormBuilder) { 
-    this.arr = [
-      { food: 'dgfdgdgfd', beauty: 'dfdfdsfds', fun : 'dfdsfdsfds', retail: 'dsgdsgdsg', cuisine: 'dsfdsgdsg', attributes: 'dgdsgds'},
-      { food: 'dgfdgdgfd', beauty: 'dfdfdsfds', fun : 'dfdsfdsfds', retail: 'dsgdsgdsg', cuisine: 'dsfdsgdsg', attributes: 'dgdsgds'},
-      { food: 'dgfdgdgfd', beauty: 'dfdfdsfds', fun : 'dfdsfdsfds', retail: 'dsgdsgdsg', cuisine: 'dsfdsgdsg', attributes: 'dgdsgds'},
-      { food: 'dgfdgdgfd', beauty: 'dfdfdsfds', fun : 'dfdsfdsfds', retail: 'dsgdsgdsg', cuisine: 'dsfdsgdsg', attributes: 'dgdsgds'},
-      { food: 'dgfdgdgfd', beauty: 'dfdfdsfds', fun : 'dfdsfdsfds', retail: 'dsgdsgdsg', cuisine: 'dsfdsgdsg', attributes: 'dgdsgds'},
-      { food: 'dgfdgdgfd', beauty: 'dfdfdsfds', fun : 'dfdsfdsfds', retail: 'dsgdsgdsg', cuisine: 'dsfdsgdsg', attributes: 'dgdsgds'}
-    ]
+  form: FormGroup 
+
+  data = [ 
+
+            { 
+    
+                "id": 17, 
+    
+                "name": "Food & Drink", 
+    
+                "category_tags": [ 
+    
+                    { 
+    
+                        "id": 2, 
+    
+                        "tag": "Fun" 
+    
+                    }, 
+    
+                    { 
+    
+                        "id": 4, 
+    
+                        "tag": "Outdoor" 
+    
+                    },
+    
+    
+                ], 
+    
+                "logo": "", 
+    
+                "image": "", 
+    
+                "description": "" 
+    
+            }, 
+    
+            { 
+    
+                "id": 15, 
+    
+                "name": "Beauty & Health", 
+    
+                "category_tags": [ 
+    
+                    { 
+    
+                        "id": 7, 
+    
+                        "tag": "test" 
+    
+                    },
+    { 
+    
+                          "id": 1, 
+      
+                          "tag": "test" 
+      
+                      } 
+    
+                ], 
+    
+                "logo": "", 
+    
+                "image": "", 
+    
+                "description": "" 
+    
+            },
+    { 
+    
+                  "id": 19, 
+      
+                  "name": "Fun & leasure", 
+      
+                  "category_tags": [ 
+      
+                      { 
+      
+                          "id": 10, 
+      
+                          "tag": "cool" 
+      
+                      }, 
+      
+                      { 
+      
+                          "id": 13, 
+      
+                          "tag": "Outdoor cool" 
+      
+                      },
+      
+      
+                  ], 
+      
+                  "logo": "", 
+      
+                  "image": "", 
+      
+                  "description": "" 
+      
+              }, 
+    
+        ];
+
+    countarray =[];
+    countOutlets = 12;
+    Color = {disable: '#757575', enable: '#FFFFFF'};
+    btn = {'color': '#757575'};
 
     
+
+  constructor(private formBuilder : FormBuilder) { 
+
+    let groupFormArray = {}
+
+    for(let i=0; i < this.data.length; i++){
+      groupFormArray[this.data[i].id] = new FormArray([]) 
+    }
+
+    this.form = this.formBuilder.group(groupFormArray);
+
+     this.addCheckboxes()
   }
 
   ngOnInit(): void {
-    this.creatSelectForm()
-  }
-
-
-
-  creatSelectForm()
-  {
-    this.form = this.formBuilder.group({
-      health : this.formBuilder.array([]),
-      beauty: this.formBuilder.array([]),
-      fun: this.formBuilder.array([]),
-      retail: this.formBuilder.array([]),
-      cuisine: this.formBuilder.array([]),
-      attributes: this.formBuilder.array([])
-    })
     
   }
 
-  get health() {
-    return this.form.controls.health as FormArray;
-  }
-  get beauty() {
-    return this.form.controls.beauty as FormArray;
-  }
-  get fun() {
-    return this.form.controls.fun as FormArray;
-  }
-  get retail() {
-    return this.form.controls.retail as FormArray;
-  }
-  get cuisine() {
-    return this.form.controls.cuisine as FormArray;
-  }
-  get attributes() {
-    return this.form.controls.attributes as FormArray;
+
+  get formArray()
+  {
+    let controls:FormArray
+    let controlsArray = []
+    for(let i=0; i < this.data.length; i++)
+    {
+      let name = this.data[i].id
+      controls =  this.form.controls[name] as FormArray
+      controlsArray.push(controls)
+    }
+    return controlsArray
   }
 
+  addCheckboxes() 
+  {
+    for(let i=0; i < this.data.length; i++)
+    {
+      let categories = this.data[i].category_tags
+      for(let j = 0; j < categories.length; j++){
+          this.formArray[i].push(new FormControl(false))
+      }
+    }
+  }
 
-
-  
-
-//   convertToValue(key: string) {
-//     debugger
-//   return this.form.value[key].map((x, i) => x && this[key][i]).filter(x => !!x);
-// }
-
-  // get tags() {
-  //   return this.form.get("tags") as FormArray
-  // }
-
-  
+  getCount($event)
+  {
+    let isChecked = $event.checked
+    if(isChecked == true){
+      this.countarray.push(isChecked)
+      
+    } else 
+    {
+      this.countarray.pop()
+    }
+    if(this.countarray.length == 0)
+    {
+      this.btn = {'color': this.Color.disable}
+    }else
+    {
+      this.btn = {'color': this.Color.enable}
+    }
+  }
 
   onLocationBack(): void 
   {
@@ -85,20 +194,29 @@ export class SelectTagsComponent implements OnInit {
 
   doSubmit()
   {
-    // const valueToStore = Object.assign({}, this.form.value, {
-    //   health: this.convertToValue('health'),
-    //   beauty: this.convertToValue('beauty'),
-    //   fun: this.convertToValue('fun'),
-    //   retail: this.convertToValue('retail'),
-    //   cuisine: this.convertToValue('cuisine'),
-    //   attributes: this.convertToValue('attributes')
+    let idArray = []
+    let selectedtagIds
+    for(let i=0; i < this.data.length; i++)
+    {
+      let id = this.data[i].id
+      let category_tags = this.data[i].category_tags
+      for(let j =0; j < category_tags.length; j++)
+      {
+        let values = this.form.value[id]
+       selectedtagIds = values.map((checked, j) => checked ? this.data[i].category_tags[j].id : null).filter(v => v !== null);
+      }
 
-    // });
-    // console.log(valueToStore);
-    console.log(this.form.value)
-  }
+      idArray.push(selectedtagIds)  
+    }
+    let mergeIds = [].concat.apply([], idArray);
 
+    let arrstring = mergeIds.map((x) => String(x))
 
-  
+    let payLoad = {
+      'tags': arrstring
+    }
+
+    console.log('mazhar', payLoad)
+    }
 
 }
